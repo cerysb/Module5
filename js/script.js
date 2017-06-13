b623a1a84f89cfe4809ff5eb332b8d8c6686fea9
CerysB <cerysb@tigereyeconsulting.com>
2017-06-13 23:13:07 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -79,11 +79,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // so it can be called when server responds with the categories data.
 
 // *** start ***
+
+// *** start ***
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -101,7 +103,11 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
+    
+
+    function chooseRandomCategory (categories) {
+    var chosenCategoryShortName = ....
+    }
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
